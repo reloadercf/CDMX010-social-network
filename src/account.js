b9c45f6e7b206ee9175aebc76a675423be6fff7f
LaubetBeltran <laubetpop@gmail.com>
2021-02-03 12:51:45 -0600

    Routing y render de las seeciones de la red social

---
+++ src/account.js
@@ -0,0 +1,12 @@
+export const account = `
+<div>LOGO</div>
+<div> 
+    <p>FORMULARIO</p>
+    <input type="text" placeholder="Nombre de usuario">
+    <input type="text" placeholder="Correo electrónico">
+    <input type="text" placeholder="Contraseña">
+    <input type="text" placeholder="Confirmar contraseña">
+    <p>La siguiente información aparecerá en tu perfil</p>
+    <input type="text" placeholder="Sobre mi" máx=20 >
+    <button>Crear cuenta</button>
+</div>`
