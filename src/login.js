3a0e0b6fb57d0ef17f494ea8c47cf6b7e51e3291
osiris <wendyosi25@gmail.com>
2021-02-04 09:13:19 -0600

    Interfaz de login

--- src/login.js
+++ src/login.js
@@ -1,8 +1,28 @@
-export const login = `<div>Imagen de usuario</div>
-<div><input type="email"></input>
-<input type="password"></input>
+export const login = `<div class="container-login">
+<div class="logo-login">
+<img src="./images/logoGris.png" alt="Logo"> 
 </div>
-<div><button> Iniciar Sesión</button></div>
-<div><a href="#">Recuperar contraseña</a></div>
-<div></div>
-`
+<div class="input-login">
+    <img class="icon-email" src="./images/icons_email.svg">
+    <input type="email" placeholder="Email" ></input><br>
+</div>
+<div class="input-login">
+    <img class="icon-password" src="./images/icon_password.svg">
+    <input type="password" placeholder="Password"></input>
+</div>
+<div class="check-login"><input type="checkbox">Recordar usuario</input></div>
+<div class="button-login">
+<button> Iniciar Sesión</button>
+<a href="#">Recuperar contraseña</a>
+</div>
+<div class="socialNet-login">
+<a>Ingresar con:</a><br>
+ <img src="./images/fb.png">
+ <img src="./images/github.png">
+ <img src="./images/google.png">
+</div>
+<div register-login>
+<p>¿No tienes cuenta? <a href="#">Regístrate</a></p>
+</div>
+
+</div>`
