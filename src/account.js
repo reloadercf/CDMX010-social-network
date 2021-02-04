df693c553a1b8afd601aa6661c19a9bbbb4df373
LaubetBeltran <laubetpop@gmail.com>
2021-02-04 00:55:23 -0600

    Maquetación de la página de Registro de usuarios// Primera parte

--- src/account.js
+++ src/account.js
@@ -1,12 +1,38 @@
 export const account = `
-<div>LOGO</div>
-<div> 
-    <p>FORMULARIO</p>
-    <input type="text" placeholder="Nombre de usuario">
-    <input type="text" placeholder="Correo electrónico">
-    <input type="text" placeholder="Contraseña">
-    <input type="text" placeholder="Confirmar contraseña">
-    <p>La siguiente información aparecerá en tu perfil</p>
-    <input type="text" placeholder="Sobre mi" máx=20 >
-    <button>Crear cuenta</button>
-</div>`
+<section id="A-root">
+<div class="A-account-container">
+    <div id="A-logo-container">
+        <img id="A-logo" src="./images/logo.svg">
+    </div>
+        <br>
+        <br>
+        <br>
+    <div id="A-form-container"> 
+        <p class="A-title">Registro</p>
+        <br>
+        <br>
+        <input id="A-input-nameUser" class="A-input-1" type="text" placeholder="Nombre de usuario">
+        <br>
+        <br>
+        <input id="A-input-mail" class="A-input-1" type="text" placeholder="Correo electrónico">
+        <br>
+        <br>
+        <input id="A-input-password" class="A-input-1" type="text" placeholder="Contraseña">
+        <br>
+        <br>
+        <input id="A-input-password-confirm" class="A-input-1" type="text" placeholder="Confirmar contraseña">
+        <br>
+        <br>
+        <label class="A-label-aboutUser">La siguiente información aparecerá en tu perfil</label>
+        <br>
+        <input class="A-input-1" type="text" placeholder="Sobre mi" máx=20 >
+        <br>
+        <br>
+        <br>
+        <button class="account-button">Crear cuenta</button>
+        <br>
+        <br>
+        <br>
+    </div>
+</div>
+</section>`
