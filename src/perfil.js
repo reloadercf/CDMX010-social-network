b6a277f33323211ca642a51b49786cb0eb02fc9b
Alejandra García <agarcia_pacheco@hotmail.com>
2021-02-04 09:04:29 -0600

    Pantalla del perfil casi lista

--- src/perfil.js
+++ src/perfil.js
@@ -1,15 +1,37 @@
-export const perfil = `<div>Nombre de usuario</div>
-    <div>Información sobre el usuario</div>
-    <input type="text"></input>
-    <button>Publicar</button>
-    <div>
-        <div>Puntos - menú</div>
-        <div>Post</div>
+export const perfil =
+    `
+    <div class="container">
+
+    <div class="imgContainer">
+    <img src="./images/user.png" class="image">
+    
+    <div class="info">
+    <div class="userName">Nombre de usuario</div>
+    <div class="about">Información sobre el usuario</div>
+    </div>
+    </div>
+
+    <div class="post">
+    <input type="text" placeholder="Nueva publicación" class="newPost"></input>
+    <button class="enter">Publicar</button>
+    </div>
+
+    <div class="activity">
+        <img src="./images/points.jpg" class="point">
+        <div class="name">Nombre del usuario</div>
+        <div class="publication">Publicación</div>
+
+        <div class="interaction">
         <div>Imagen</div>
         <div>Comentarios</div>
         <div>Número de comentarios</div>
         <div>Likes</div>
         <div>Número de likes</div>
+        </div>
+
     </div>
-    <input type="text"></input>
-    <button>Comentar</button>`
+
+    <input type="text" class="nextPost"></input>
+    <button class="enter">Comentar</button>
+
+    </div>`
