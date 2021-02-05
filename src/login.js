35a51bb2edf4288320e37645efb188cee56ea974
osiris <wendyosi25@gmail.com>
2021-02-05 13:04:19 -0600

    codigo css de login actualizado

--- src/login.js
+++ src/login.js
@@ -1,18 +1,18 @@
 export const login = `<div class="container-login">
 <div class="logo-login">
-<img src="./images/logoGris.png" alt="Logo"> 
+<img id="A-logo" src="./images/logo.svg" alt="Logo"> 
 </div>
 <div class="input-login">
-    <img class="icon-email" src="./images/icons_email.svg">
-    <input type="email" placeholder="Email" ></input><br>
+    <input id="A-input-mail" class="A-input-1" type="email" maxlength=50 placeholder="Email" ></input><br>
 </div>
 <div class="input-login">
-    <img class="icon-password" src="./images/icon_password.svg">
-    <input type="password" placeholder="Password"></input>
+    <input id="A-input-password" class="A-input-1" type="password" maxlength=16 placeholder="Password"></input>
 </div>
-<div class="check-login"><input type="checkbox">Recordar usuario</input></div>
+<div class="check-login">
+<br>
+<input type="checkbox">Recordar usuario</input></div>
 <div class="button-login">
-<button> Iniciar Sesión</button>
+<button class="account-button"> Iniciar Sesión</button><br>
 <a href="#">Recuperar contraseña</a>
 </div>
 <div class="socialNet-login">
@@ -21,9 +21,8 @@ export const login = `<div class="container-login">
  <img src="./images/github.png">
  <img src="./images/google.png">
 </div>
-<div register-login>
+<div class="register-login">
 <p>¿No tienes cuenta? <a href="#">Regístrate</a></p>
 </div>
 
 </div>`
-
