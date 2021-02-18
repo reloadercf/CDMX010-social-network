94c1d9c23a7fda4815c0410419395a6ea8c2d4ca
MiriamNM <miriamnavamucino@gmail.com>
2021-02-18 10:16:29 -0600

    routing the first history

---
+++ src/data.js
@@ -0,0 +1,25 @@
+//this is the enter from app
+//import {onNavigate, newDiv} from './routers';
+//import {myFunction} from './lib/home.js'
+//import { theHome1 } from './main.js'
+//import {onNavigate} from './routers.js'
+//import {theLogin } from './main.js'
+//myFunction();
+//theHome();
+//import { theHome } from './lib/home.js'
+//import { login } from './lib/login.js';
+
+//myFunction();
+
+//window.addEventListener('load', theHome1());
+/*window.addEventListener('DOMContentLoaded', () => { 
+    auth.onAuthStateChanged(function(user) {
+        if (user) {
+            onNavigate('/home')
+        } else {
+            onNavigate('/')
+        }
+        })
+});*/
+
+
---
+++ src/lib/wall.js
@@ -0,0 +1,26 @@
+//This is the wall
+export const wall = () => {
+    let html3 =`
+    <div class='wall'>
+    <div>
+    <img src='src/assets/logo-jalo.png'>
+    <img src='src/assets/logo-jalo.png'>
+    </div>
+    <div>
+    <input type='text' placeholder='Escribe...'></input>
+    <button id='hold'>Publicar</button>
+    </div>
+    <div>
+    <input type='text' placeholder='Escribe...'></input>
+    <button id='hold'>Publicar</button>
+    </div>
+    <div>
+    <section id='theText'></section>
+    <img src='src/assets/me-enviaja.png'>
+    </div>    
+    </div>
+    `
+    return html3
+} 
+
+//faltan las promesas
