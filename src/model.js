15a8329ae2bd140c8e356b1ff90e55eb95cc31f7
blancadennis <lblancadennis@gmail.com>
2021-02-22 13:06:15 -0600

    firebase instalacion y funcionalidades en documentos js correspondientes

--- src/model.js
+++ src/model.js
@@ -1,5 +1,4 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 var firebaseConfig = {
   apiKey: "AIzaSyAphkTjnCyuMEe9J2BlkLSnRf11LDrRKq8",
   authDomain: "jaloredsocial.firebaseapp.com",
@@ -12,48 +11,3 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    var uid = user.uid;
-    // ...
-    document.getElementById("login").innerHTML="Binvenid@"+user.email;
-    console.log(user);
-  } else {
-    // User is signed out
-    // ...
-    document.getElementById("login").innerHTML="No logeado "+user.email;
-  }
-});
-function enviar (){
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-  //alert("email="+email+" password="+password);
-  firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((user) => {
-      // Signed in
-      // ...
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ..
-      alert(errorMessage);
-    });
-}
-function acceso (){
-  var emailLog = document.getElementById('emailLog').value;
-  var passwordLog = document.getElementById('passwordLog').value;
-  //alert("email="+email+" password="+password);
-  firebase.auth().signInWithEmailAndPassword(emailLog, passwordLog)
-    .then((user) => {
-      // Signed in
-      // ...
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      alert(errorMessage);
-    });
-}
