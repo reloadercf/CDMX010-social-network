5c55b4c1da2ada54b99f3764ecab2cd7402b9feb
Alejandra García <agarcia_pacheco@hotmail.com>
2021-03-02 13:12:18 -0600

    iniciando a estructurar test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,16 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+// import { renderRoutes } from '../src/routing.js';
+import { login } from '../src/login.js'
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+
+
+describe('navegacion', () => {
+	// const navegacion = renderRoutes('/');
+	const loginPage = login;
+//	console.log(navegacion);
+	console.log(loginPage);
+  it('Asegurarnos que el usuario pueda navegar entre las pantallas', () => {
+    expect(navegacion.outerHTML).toBe(loginPage);
   });
 });
+
