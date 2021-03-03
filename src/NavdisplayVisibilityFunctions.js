3181b7362199c5476bb84b6f162a3ad179615fe5
LaubetBeltran <laubetpop@gmail.com>
2021-03-02 21:30:12 -0600

    Arreglando estilos del navbar// parte 2// pasan test

--- src/NavdisplayVisibilityFunctions.js
+++ src/NavdisplayVisibilityFunctions.js
@@ -1,24 +1,24 @@
-// LINKS DEL NAV//
-const logoutLink = document.getElementById('logout');
-const loginLink = document.getElementById('log');
-const accountLink = document.getElementById('acc');
+// LI-LINKS DEL NAV//
+const logoutLinkLi = document.getElementById('logoutLinkLi');
+const loginLinkLi = document.getElementById('loginLinkLi');
+const accountLinkLi = document.getElementById('accountLinkLi');
 const homeLink = document.getElementById('hom');
-const perfilLink = document.getElementById('per');
+const perfilLinkLi = document.getElementById('perfilLinkLi');
 
 // Cuando el usuario se logea y entra al home esto es lo que ve:
 export const navLinkVisibilityLogin = () => {
-  logoutLink.style.display = 'block';
+  logoutLinkLi.style.display = 'block';
   homeLink.style.display = 'block';
-  perfilLink.style.display = 'block';
-  loginLink.style.display = 'none';
-  accountLink.style.display = 'none';
+  perfilLinkLi.style.display = 'block';
+  loginLinkLi.style.display = 'none';
+  accountLinkLi.style.display = 'none';
 };
 
 // Cuando el usuario sale del home esto es lo que se ve
 export const navLinkVisibilityWithoutLogin = () => {
-  logoutLink.style.display = 'none';
+  logoutLinkLi.style.display = 'none';
   homeLink.style.display = 'none';
-  perfilLink.style.display = 'none';
-  loginLink.style.display = 'block';
-  accountLink.style.display = 'block';
+  perfilLinkLi.style.display = 'none';
+  loginLinkLi.style.display = 'block';
+  accountLinkLi.style.display = 'block';
 };
